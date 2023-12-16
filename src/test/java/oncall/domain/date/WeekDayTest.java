b08e5fa3a20711233a61d4e70b76830b9977b830
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-12-16 16:47:37 +0900

    test: 요일 클래스 테스트 생성

--- src/test/java/oncall/domain/date/WeekDayTest.java
+++ src/test/java/oncall/domain/date/WeekDayTest.java
@@ -7,8 +7,23 @@ import org.junit.jupiter.api.Test;
 
 class WeekDayTest {
     @Test
+    @DisplayName("요일 조회에 성공한다.")
+    public void getWeekDayTest() {
+        //given&when&then
+        assertAll(
+                () -> assertEquals(WeekDay.Monday.getWeekday(), "월"),
+                () -> assertEquals(WeekDay.Tuesday.getWeekday(), "화"),
+                () -> assertEquals(WeekDay.Wednesday.getWeekday(), "수"),
+                () -> assertEquals(WeekDay.Thursday.getWeekday(), "목"),
+                () -> assertEquals(WeekDay.Friday.getWeekday(), "금"),
+                () -> assertEquals(WeekDay.Saturday.getWeekday(), "토"),
+                () -> assertEquals(WeekDay.Sunday.getWeekday(), "일")
+        );
+    }
+
+    @Test
     @DisplayName("다음 요일 조회에 성공한다.")
-    public void getNextWeekDayTest() throws Exception {
+    public void getNextWeekDayTest() {
         //given
         WeekDay now = WeekDay.Sunday;
 
