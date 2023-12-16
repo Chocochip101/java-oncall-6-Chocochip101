72ad5332dd6785ba7a0ead88d43eb3112dcf7004
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-12-16 14:40:11 +0900

    feat: 평일 비상 근무자 닉네임 입력 중복 검사 구현

--- src/main/java/oncall/ui/InputValidation.java
+++ src/main/java/oncall/ui/InputValidation.java
@@ -3,7 +3,9 @@ package oncall.ui;
 import static oncall.global.ErrorMessage.INVALID_DATE_ERROR;
 import static oncall.global.ErrorMessage.INVALID_NICKNAME_ERROR;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import oncall.global.Config;
 
 public class InputValidation {
@@ -16,8 +18,22 @@ public class InputValidation {
     }
 
     static void validNickNames(List<String> nicknames) {
+        checkMaxLength(nicknames);
+        checkDuplication(nicknames);
+    }
+
+    private static void checkMaxLength(List<String> nicknames) {
         for (String nickname : nicknames) {
             if (nickname.length() > Config.NICKNAME_MAX_SIZE) {
+                throw new IllegalArgumentException(INVALID_NICKNAME_ERROR.getMessage());
+            }
+        }
+    }
+
+    private static void checkDuplication(List<String> nicknames) {
+        Set<String> uniqueNicknames = new HashSet<>();
+        for (String nickname : nicknames) {
+            if (!uniqueNicknames.add(nickname)) {
                 throw new IllegalArgumentException(INVALID_NICKNAME_ERROR.getMessage());
             }
         }
